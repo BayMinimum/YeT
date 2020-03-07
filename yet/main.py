0e2ee5d0052688f983a8b872c97eacbab7bf2b99
Sangbum Kim <bearksb1115@gmail.com>
2020-03-07 13:19:29 +0900

    add directory processing feature

--- yet/main.py
+++ yet/main.py
@@ -8,7 +8,49 @@ from yet.loader import load_yet
 logger = logging.getLogger()
 
 
-def step(in_path, out_path):
+def convert_extension(filename, extensions=('yaml', 'yml',)):
+    """
+    Convert YeT extension in filename to .tex
+
+    :param filename: string of file name. validity not checked.
+    :param extensions: tuple of extensions (without dot) to treat as YeT
+    """
+    # change YAML extension to .tex
+    if '.' in filename:
+        ext = filename.split('.')[-1]
+        if ext in extensions:
+            idx = filename.rindex('.')
+            return f'{filename[:idx]}.tex'
+    # or append .tex if appropriate extension is not found
+    return f'{filename}.tex'
+
+
+def process_dir(in_path, out_path, extensions=('yaml', 'yml',)):
+    """
+    Read from in_path, convert YeT to TeX, and write to out_path
+
+    :param in_path: string of existent directory path
+    :param out_path: string of valid directory path, or sys.stdout
+    :param extensions: tuple of extensions (without dot) to treat as YeT
+    """
+    if isinstance(out_path, str):
+        os.makedirs(out_path, mode=0o644, exist_ok=True)
+
+    for root, _, files in os.walk(in_path):
+        logger.info(f'entering directory {root}')
+        for name in files:
+            if '.' in name and name.split('.')[-1] in extensions:
+                _in_path = os.path.join(root, name)
+                logger.info(f'found YeT file {_in_path}')
+                if out_path is sys.stdout:
+                    _out_path = sys.stdout
+                else:
+                    _out_path = os.path.join(root.replace(in_path, out_path, 1),
+                                             convert_extension(name, extensions))
+                process_file(_in_path, _out_path)
+
+
+def process_file(in_path, out_path):
     """
     Read from in_path, convert YeT to TeX, and write to out_path
 
@@ -58,29 +100,16 @@ def main():
 
     if args.input == '-':
         in_path = sys.stdin
+        out_path = sys.stdout if args.output in (None, '-') else args.output
     elif os.path.exists(args.input):
-        if os.path.isfile(args.input):
-            in_path = args.input
-        else:
-            raise NotImplementedError('Directory batch-processing is not supported yet')
+        in_path = args.input
+        if os.path.isdir(in_path):
+            out_path = sys.stdout if args.output == '-'\
+                                  else in_path if args.output is None\
+                                  else args.output
+            return process_dir(in_path, out_path)
+        out_path = sys.stdout if args.output == '-' else convert_extension(args.output)
     else:
         raise FileNotFoundError(f'Path {args.input} does not exist')
 
-    if args.output == '-':
-        out_path = sys.stdout
-    elif args.output is None:
-        if args.input == '-':
-            out_path = sys.stdout
-        else:
-            # change YAML extension to .tex
-            if args.input.endswith('.yaml'):
-                out_path = f'{args.input[:-5]}.tex'
-            elif args.input.endswith('.yml'):
-                out_path = f'{args.input[:-4]}.tex'
-            # or append .tex if appropriate extension is not found
-            else:
-                out_path = f'{args.input}.tex'
-    else:
-        out_path = args.output
-
-    step(in_path, out_path)
+    process_file(in_path, out_path)
